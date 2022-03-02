afe1eabcb3002032513acfbe7834230b49e9a8a6
reesericci <me@reeseric.ci>
2022-03-02 12:55:21 -0600

    Use svelte component for header!

---
+++ Hero/src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+	export { text, strikethrough };
+	import { onMount, afterUpdate } from 'svelte';
+	import { blur } from 'svelte/transition'
+	
+	let text = [];
+	let strikethrough = []
+
+	const strikethoughSet = new Set()
+	let currentIndex = 0;
+	
+	onMount(() => {
+		strikethrough.forEach((el) => {
+			strikethoughSet.add(el)
+		})
+
+		let interval = setInterval(updateText, 1000)
+	});
+		
+	function updateText() {
+		if(currentIndex == text.length - 1) {
+			currentIndex = 0;
+		} else { 
+			currentIndex += 1
+		}
+	}
+	
+	afterUpdate(() => {
+		if(strikethoughSet.has(text[currentIndex])) {
+			document.getElementById("animatedText").style.textDecoration = "line-through";
+		}
+	})
+
+</script>
+
+<main>
+	<h1>
+	say NO to	
+	</h1>
+	{#key currentIndex}
+		<h1 id="animatedText" in:blur="{{amount: 10}}">
+			{text[currentIndex]}
+		</h1>
+	{/key}
+</main>
+
+<style>
+	@import url('https://fonts.cdnfonts.com/css/neue-haas-grotesk-text-pro');
+	main {
+			height: 100vh;
+			width: 100vw;
+			position: relative;
+	}
+	main, h1 {
+			font-size: 20vw;
+			font-family: 'Neue Haas Grotesk Text Pro';
+			font-weight: 700;
+			color: #FF0000;
+			margin-left: 5px;
+	}
+	
+	@media only screen and (max-width: 800px) {
+			main {
+					visibility: hidden;
+					height: inherit;
+			}
+			main, h1 {
+					visibility: visible;
+			}
+	} 
+</style>
